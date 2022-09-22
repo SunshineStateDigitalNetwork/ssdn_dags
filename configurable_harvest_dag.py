43e606e40c43d33386d332b1fadbeb0ad0885c5e
Matthew Miguez <r.m.miguez@gmail.com>
2022-09-22 09:34:35 -0700

    single partner dags

--- configurable_harvest_dag.py
+++ configurable_harvest_dag.py
@@ -18,7 +18,7 @@ sys.path.insert(0, PATH)
 import ssdn_assets
 
 
-with DAG('ssdn_dynamic_harvest',
+with DAG('ssdn_single_harvest',
          default_args={'depends_on_past': False,
                        'email': ['airflow.example.org'],
                        'email_on_failure': False,
@@ -27,8 +27,8 @@ with DAG('ssdn_dynamic_harvest',
                        'retry_delay': timedelta(minutes=5),
                        'env': {'MANATUS_CONFIG': SSDN_ENV}
                        },
-         description='Dynamic harvest test',
-         tags=['ssdn', 'harvest', 'transform', 'dynamic', ],
+         description='Single partner harvest',
+         tags=['ssdn', 'harvest', ],
          start_date=datetime(2022, 1, 1),
          schedule_interval='@quarterly',
          catchup=False,
@@ -46,18 +46,12 @@ with DAG('ssdn_dynamic_harvest',
 
     clean_up = BashOperator(
         task_id='clean_up',
-        bash_command=f'rm -rf {ssdn_assets.OAI_PATH}/*/*.xml',
+        bash_command=f'rm -rf {ssdn_assets.OAI_PATH}/{{ dag_run.conf["partner"] }}/*.xml',
     )
 
-    for partner in ssdn_assets.list_config_keys(ssdn_assets.harvest_parser):
-        partner_harvest = BashOperator(
-            task_id=f'harvest_{partner}',
-            bash_command=f'python3 -m manatus --profile ssdn harvest -s {partner}',
-        )
+    partner_harvest = BashOperator(
+        task_id=f'single_harvest',
+        bash_command=f'python3 -m manatus --profile ssdn harvest -s {{ dag_run.conf["partner"] }}',
+    )
 
-        partner_transform = BashOperator(
-            task_id=f'transform_{partner}',
-            bash_command=f'python3 -m manatus --profile ssdn transform -s {partner}',
-        )
-
-        chain([repo_update, clean_up], partner_harvest, partner_transform, [s3_upload])
+    chain([repo_update, clean_up], partner_harvest, [s3_upload])
--- configurable_transform_dag.py
+++ configurable_transform_dag.py
@@ -18,7 +18,7 @@ sys.path.insert(0, PATH)
 import ssdn_assets
 
 
-with DAG('ssdn_dynamic_harvest',
+with DAG('ssdn_single_transform',
          default_args={'depends_on_past': False,
                        'email': ['airflow.example.org'],
                        'email_on_failure': False,
@@ -27,8 +27,8 @@ with DAG('ssdn_dynamic_harvest',
                        'retry_delay': timedelta(minutes=5),
                        'env': {'MANATUS_CONFIG': SSDN_ENV}
                        },
-         description='Dynamic harvest test',
-         tags=['ssdn', 'harvest', 'transform', 'dynamic', ],
+         description='Single partner transform',
+         tags=['ssdn', 'transform', ],
          start_date=datetime(2022, 1, 1),
          schedule_interval='@quarterly',
          catchup=False,
@@ -44,20 +44,9 @@ with DAG('ssdn_dynamic_harvest',
         bash_command='whoami',
     )
 
-    clean_up = BashOperator(
-        task_id='clean_up',
-        bash_command=f'rm -rf {ssdn_assets.OAI_PATH}/*/*.xml',
+    partner_transform = BashOperator(
+        task_id=f'sing_transform',
+        bash_command=f'python3 -m manatus --profile ssdn transform -s {{ dag_run.conf["partner"] }}',
     )
 
-    for partner in ssdn_assets.list_config_keys(ssdn_assets.harvest_parser):
-        partner_harvest = BashOperator(
-            task_id=f'harvest_{partner}',
-            bash_command=f'python3 -m manatus --profile ssdn harvest -s {partner}',
-        )
-
-        partner_transform = BashOperator(
-            task_id=f'transform_{partner}',
-            bash_command=f'python3 -m manatus --profile ssdn transform -s {partner}',
-        )
-
-        chain([repo_update, clean_up], partner_harvest, partner_transform, [s3_upload])
+    chain(repo_update, partner_transform, s3_upload)
