54861cc0c1413d08321ec7777de1758619356c5f
mrmiguez <r.m.miguez@gmail.com>
2024-02-22 10:18:04 -0800

    fix for cleaning empty XML elements from source files

---
+++ ssdn_assets/clean_xml.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+command -v xmlstarlet >/dev/null 2>&1 || { echo >&2 "I require xmlstarlet but it's not installed.  Aborting."; exit 1; }
+
+if [ -d .back_up ]; then rm -Rf .back_up; fi
+
+clean_xml_file () {
+  xmlstarlet tr "$dirname"/xslt/stripEmptyElements.xsl .back_up/"$1" >> "$1"
+  if [[ $? -gt 0 ]]
+    then echo "Cleaning failed: $1" && return 1
+    else return 0
+  fi
+}
+
+rewind_dir () {
+  mv .back_up/"$1" "$1"
+}
+
+dirname=$(dirname "$0")
+#echo "$dirname"
+
+cd "$1" || exit
+mkdir .back_up
+for f in $( ls *.xml ); do
+  echo "Moving & cleaning $f"
+  mv "$f" .back_up/
+  clean_xml_file "$f"
+  if [[ $? -gt 0 ]]
+    then echo "Rewinding $f" && rewind_dir "$f" && exit 1
+  fi
+done
+
+rm -rf .back_up
